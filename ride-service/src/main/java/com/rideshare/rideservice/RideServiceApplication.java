ebc8a13c8cbaf3edf21b39d244d620e568c1eda7
mohamedabbouda <mohamedabbouda54@gmail.com>
2026-04-27 22:52:09 +0200

    update commit

---
+++ ride-service/src/main/java/com/rideshare/rideservice/RideServiceApplication.java
@@ -0,0 +1,16 @@
+package com.rideshare.rideservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideServiceApplication.class, args);
+	}
+
+}
+
+
+
